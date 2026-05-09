397cec2e114a8fbba4836caa68508ea0bef71c10
Roger Chappel <miscanalysis@gmail.com>
2026-05-09 18:36:57 +1000

    feat: expose handoffpad library API

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { createBundle } from "./create.js";
+export { renderMarkdown } from "./render.js";
+export type { CreateOptions, HandoffBundle, HandoffCommand, HandoffFileChange, ValidationIssue } from "./schema.js";
+export { hasErrors, readBundle, validateBundle } from "./validate.js";
+export { redactText } from "./redact.js";
